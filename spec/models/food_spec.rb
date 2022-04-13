8ec2a82f8af213e7231888cf05f475c9a889ebdb
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-13 16:02:26 +0000

    Add valid factory spec for Food model

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:food)).to be_valid
+  end
+  
   it 'is valid with a name and a description' do
     food = Food.new(
       name: 'Nasi Uduk',
