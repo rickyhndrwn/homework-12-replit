2bfbd898997dbc986c84c7c273420ae25ed1f0ba
rickyhndrwn <ricky.hndrwn98@gmail.com>
2022-04-13 15:45:22 +0000

    Add validation for Food, should return a sorted array of results that match

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,8 @@
 class Food < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
+
+  def self.by_letter(letter)
+    where("name LIKE ?", "#{letter}%").order(:name)
+  end
 end
